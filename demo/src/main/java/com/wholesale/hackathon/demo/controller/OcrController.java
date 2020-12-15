3978145501e7bdc59f906dd76a8051838bc7f5ba
Megha Agarwal <krishagarwal.0493@gmail.com>
2020-12-16 00:04:15 +0530

    d

--- demo/src/main/java/com/wholesale/hackathon/demo/controller/OcrController.java
+++ demo/src/main/java/com/wholesale/hackathon/demo/controller/OcrController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class OcrController {
 	private DetectService detectService;
 	
 	@PostMapping("/ocr")
-	public ResponseEntity<String> detectText(OcrPathDto ocrPathDto)  {
+	public ResponseEntity<String> detectText(@RequestBody OcrPathDto ocrPathDto)  {
 		
 		ResponseEntity<String> result=new ResponseEntity<>("Successfull",HttpStatus.OK);
 		try {
