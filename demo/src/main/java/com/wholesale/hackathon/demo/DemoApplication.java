f824a28f56f1c155fa583892f41820c683d7e379
Megha Agarwal <krishagarwal.0493@gmail.com>
2020-12-16 21:35:13 +0530

    fixed cors error for local host

--- demo/src/main/java/com/wholesale/hackathon/demo/DemoApplication.java
+++ demo/src/main/java/com/wholesale/hackathon/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -17,4 +19,13 @@ public class DemoApplication {
 		return new RestTemplate();
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
